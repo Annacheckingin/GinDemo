8d12ccdc3c5dbdcab2dea53183fd45cb681cf52a
LiZhengGuo <1519794332@qq.com>
2024-08-29 10:49:24 +0800

    build:解决了上一个编译上的错误

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -8,26 +8,18 @@ import (
 	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 )
+
 func Init(gin *gin.Engine) {
 
 }
-
-
 
 func JWT(u *user.User) *jwt.GinJWTMiddleware {
 	return initParams(u)
 }
 
-
-func Validate(u *user.User) bool{
+func Validate(u *user.User) bool {
 	return false
 }
-
-
-
-
-
-
 
 func initParams(u *user.User) *jwt.GinJWTMiddleware {
 
@@ -48,13 +40,15 @@ func initParams(u *user.User) *jwt.GinJWTMiddleware {
 func identityHandler(u *user.User) func(*gin.Context) (interface{}, error) {
 	return func(c *gin.Context) (interface{}, error) {
 		t := reflect.TypeOf(u)
-        for i := 0; i < t.NumField(); i++ {
-         field := t.Field(i)
-         jsonTag := field.Tag.Get("json")
-         if jsonTag == "name" {
-		  value := reflect.ValueOf(u).Field(i).Interface()
-		   return value, nil
-	     }
-		 return nil, nil
+		for i := 0; i < t.NumField(); i++ {
+			field := t.Field(i)
+			jsonTag := field.Tag.Get("json")
+			if jsonTag == "name" {
+				value := reflect.ValueOf(u).Field(i).Interface()
+				return value, nil
+			}
+			return nil, nil
+		}
+		return nil, nil
 	}
 }
